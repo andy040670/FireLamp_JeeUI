467e66a4b18fb4a0f08bb56d101f8b4f110ae976
andy040670 <andy040670@yandex.ru>
2022-01-04 21:08:12 +0300

    Update Builder.bat

--- Builder.bat
+++ Builder.bat
@@ -8,56 +8,56 @@ if exist "%USERPROFILE%\Python\python.exe" (
 )
 PATH=%PATH%;%workdir%;%USERPROFILE%\.platformio\penv\Scripts;%PYTHONPATH%;
 @chcp 1251>nul
-mode con: cols=88 lines=48
+mode con: cols=88 lines=45
 cls 
+
+if not exist "%workdir%\out_bin" (mkdir %workdir%\out_bin)
 
 :m1
 Echo  #------------------------------------------#-----------------------------------------# 
-Echo  *                COMMANDS                  *  (Russian)      КОМАНДЫ                 *
+Echo  *                COMMANDS                  *  (Russian)      пїЅпїЅпїЅпїЅпїЅпїЅпїЅ                 *
 Echo  #------------------------------------------#-----------------------------------------# 
-Echo  *              Switch/update               *          Переключить\обновить           *
-Echo  *  to MASTER branch                 - (1)  *  на ветку MASTER                        *
-Echo  *  to DEV branch                    - (2)  *  на ветку DEV                           *
+Echo  *  Switch/update - MASTER branch    - (1)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ\пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ- пїЅпїЅпїЅпїЅпїЅ MASTER     *
+Echo  *  Switch/update - DEV branch       - (2)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ\пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ- пїЅпїЅпїЅпїЅпїЅ DEV        *
 Echo  #------------------------------------------#-----------------------------------------# 
-echo  *  Reset changes in local repo!     - (3)  *  Сбросить изменения в локальном репо!   *
-Echo  *  WARNING! This will revert all changes!  *  ВНИМАНИЕ! Это сбросит все изменения!   *
+echo  *  Reset changes in local repo!     - (3)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ!   *
+Echo  *  WARNING! This will revert all changes!  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ! пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ!   *
 Echo  #------------------------------------------#-----------------------------------------# 
-Echo  *              Build only for              *              Собрать для                *
-Echo  *  Esp8266 160MHz                   - (4)  *  Esp8266 на 160МГц                      *
-Echo  *  Esp8266 80MHz                    - (5)  *  Esp8266 на 80МГц                       *
-Echo  *  Esp32                            - (6)  *  Esp32                                  *
+Echo  *                Build only                *              пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ              *
+Echo  *  Build - Esp8266 160MHz           - (4)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ Esp8266 пїЅпїЅ 160пїЅпїЅпїЅ          *
+Echo  *  Build - Esp8266 80MHz            - (5)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ Esp8266 пїЅпїЅ 80пїЅпїЅпїЅ           *
+Echo  *  Build - Esp32                    - (6)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ Esp32                      *
 Echo  #------------------------------------------#-----------------------------------------#
-Echo  *           Build and flash for            *          Сборка и прошивка для          *
-Echo  *  Esp8266 160MHz                   - (7)  *  Esp8266 на 160МГц                      *
-Echo  *  Esp8266 80MHz                    - (8)  *  Esp8266 на 80МГц                       *
-Echo  *  Esp32                            - (9)  *  Esp32                                  *
+Echo  *             Build and flash              *            пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ            *
+Echo  *  Build and upload - Esp8266@160   - (7)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ - Esp8266 пїЅпїЅ 160пїЅпїЅпїЅ  *
+Echo  *  Build and upload - Esp8266@80    - (8)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ - Esp8266 пїЅпїЅ 80пїЅпїЅпїЅ   *
+Echo  *  Build and upload - Esp32         - (9)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ - Esp32              *
 Echo  #------------------------------------------#-----------------------------------------#
-Echo  *         Build and flash (DEBUG)          *      Сборка и прошивка  (С ЛОГОМ)       *
-Echo  *  Esp8266 160MHz                  - (7d)  *  Esp8266 на 160МГц                      *
-Echo  *  Esp8266 80MHz                   - (8d)  *  Esp8266 на 80МГц                       *
-Echo  *  Serial port monitor              - (d)  *  Вывод отладочной информации (ЛОГ)      *
+Echo  *         Build and flash (DEBUG)          *      пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ  (пїЅ пїЅпїЅпїЅпїЅпїЅ)       *
+Echo  *  Build and upload - Esp8266@160   - (7D) *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ - Esp8266 пїЅпїЅ 160пїЅпїЅпїЅ  *
+Echo  *  Build and upload - Esp8266@80    - (8D) *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ - Esp8266 пїЅпїЅ 80пїЅпїЅпїЅ   *
+Echo  *  Serial port monitor               - (D) *  пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ (пїЅпїЅпїЅ)      *
 Echo  #------------------------------------------#-----------------------------------------#
-Echo  *              File System                 *            Файловая Система             *
-Echo  *  Update FS data from framework    - (u)  *  Обновить файлы ФС из фреймворка        *
-Echo  *  ** ESP8266                              *  ** ESP8266                             *
-Echo  *  Build File System image          - (b)  *  Собрать образ Файловой Системы         *
-Echo  *  Build and upload File System     - (f)  *  Собрать и прошить Файловую Систему     *
-Echo  *  ** ESP32 -----                          * ** ESP32 ------                         *
-Echo  *  Build File System image         - (b1)  *  Собрать образ Файловой Системы         *
-Echo  *  Build and upload File System    - (f1)  *  Собрать и прошить Файловую Систему     *
+Echo  *  Update FS data from framework    - (u)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ        *
+Echo  *          FS for ESP8266                  *              пїЅпїЅпїЅ ESP8266                *
+Echo  *  Build File System image          - (b)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ         *
+Echo  *  Build and upload File System     - (f)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ     *
+Echo  *            FS for ESP32                  *                пїЅпїЅпїЅ ESP32                *
+Echo  *  Build File System image         - (b1)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ         *
+Echo  *  Build and upload File System    - (f1)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ     *
 Echo  #------------------------------------------#-----------------------------------------#
-Echo  *  Erase Flash ESP8266              - (e)  *  Стереть флеш контроллера ESP8266       *
-Echo  *  Erase Flash ESP32               - (e1)  *  Стереть флеш контроллера ESP32         *
+Echo  *  Erase Flash    ESP8266           - (e)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ   ESP8266     *
+Echo  *  Erase Flash    ESP32            - (e1)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ   ESP32       *
 Echo  #------------------------------------------#-----------------------------------------#
-Echo  *  Update libs and PIO Core         - (g)  *  Обновить библиотеки и среду PIO Core   *
-Echo  *  Clean up temp files .pio         - (c)  *  Очистить временные файлы .pio          *
+Echo  *  Update libs and PIO Core         - (g)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅ PIO Core   *
+Echo  *  Clean up temp files .pio         - (c)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ .pio          *
 Echo  *------------------------------------------#-----------------------------------------*
-Echo  *  CMD window                       - (m)  *  Открыть окно коммандной строки CMD     *
+Echo  *  CMD window                       - (m)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ CMD     *
 Echo  *------------------------------------------#-----------------------------------------*
-Echo  *  Remove Platformio installation   - (R)  *  Полностью удалить Platformio с ПК      *
+Echo  *  Remove Platformio installation   - (R)  *  пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ Platformio пїЅ пїЅпїЅ      *
 Echo  #------------------------------------------#-----------------------------------------#
 Echo.
-Set /p choice="Your choice (Ваш выбор): " 
+Set /p choice="Your choice (пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ): "
 
 
 if "%choice%"=="1" (
@@ -71,26 +71,39 @@ if "%choice%"=="2" (
 if "%choice%"=="3" call update-DEV-from-Git.cmd 3
 if "%choice%"=="4" (
 	"%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --environment esp8266@160
+	copy %workdir%\.pio\build\esp8266@160\firmware.bin %workdir%\out_bin
 )
-if "%choice%"=="5" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --environment esp8266)
-if "%choice%"=="6" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --environment esp32)
+if "%choice%"=="5" (
+	"%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --environment esp8266
+	copy %workdir%\.pio\build\esp8266\firmware.bin %workdir%\out_bin
+)
+if "%choice%"=="6" (
+	"%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --environment esp32
+	copy %workdir%\.pio\build\esp32\firmware.bin %workdir%\out_bin
+)
 if "%choice%"=="7" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target upload --environment esp8266@160)
 if "%choice%"=="8" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target upload --environment esp8266)
 if "%choice%"=="9" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target upload --environment esp32)
-if "%choice%"=="7d" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target upload --environment esp8266@160dev)
-if "%choice%"=="8d" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target upload --environment esp8266dev)
-if "%choice%"=="d" (start %workdir%\SerialMonitor.cmd)
+if "%choice%"=="7D" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target upload --environment esp8266@160dev)
+if "%choice%"=="8D" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target upload --environment esp8266dev)
+if "%choice%"=="D" (start %workdir%\SerialMonitor.cmd)
 if "%choice%"=="u" (
 	cd %workdir%\resources\
 	start respack.cmd
 	cd %workdir%
 )
-if "%choice%"=="b" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target buildfs --environment esp8266@160)
+if "%choice%"=="b" (
+	"%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target buildfs --environment esp8266@160
+	copy %workdir%\.pio\build\esp8266@160\littlefs.bin %workdir%\out_bin
+)
 if "%choice%"=="f" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target uploadfs --environment esp8266@160)
-if "%choice%"=="b1" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target buildfs --environment esp32)
-if "%choice%"=="f1" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target uploadfs --environment esp32)
 if "%choice%"=="e" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target erase --environment esp8266@160)
-if "%choice%"=="e1 ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target erase --environment esp32)
+if "%choice%"=="b1" (
+	"%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target buildfs --environment esp32
+	copy %workdir%\.pio\build\esp32\spiffs.bin %workdir%\out_bin
+)
+if "%choice%"=="f1" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target uploadfs --environment esp32)
+if "%choice%"=="e1" ("%USERPROFILE%\.platformio\penv\Scripts\pio.exe" run --target erase --environment esp32)
 if "%choice%"=="c" (
 	pio system prune -f
 	rmdir /S /Q %workdir%\.pio
